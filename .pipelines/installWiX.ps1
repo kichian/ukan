50a5d69cf828259c24fc1d7962e6711bb5fa51d5
kic <107183332+kichian@users.noreply.github.com>
2025-10-05 18:57:40 +0800

    Update installWiX.ps1

--- .pipelines/installWiX.ps1
+++ .pipelines/installWiX.ps1
@@ -24,3 +24,24 @@ Start-Process -Wait -FilePath "$($ENV:Temp)\wix314.exe" -ArgumentList "/install 
 # Extract WiX binaries and copy wix.targets to the installed dir
 Expand-Archive -Path "$($ENV:Temp)\wix314-binaries.zip" -Force -DestinationPath "$($ENV:Temp)"
 Copy-Item -Path "$($ENV:Temp)\wix.targets" -Destination "C:\Program Files (x86)\WiX Toolset v3.14\"
+<databaseChangeLog
+        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+	  http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.8.xsd">
+
+    <changeSet id="V0.12-20220424-001" author="Chambers" labels="space">
+        <comment>Space Invite Record Table add space_id index</comment>
+        <sql>
+            ALTER TABLE `${table.prefix}space_invite_record`
+            ADD INDEX `idx_space_id`(`invite_space_id`) USING BTREE;
+        </sql>
+    </changeSet>
+
+    <changeSet id="V0.12-20220424-002" author="Chambers" labels="space">
+        <comment>Space Invite Record Table add email index</comment>
+        <sql>
+            ALTER TABLE `${table.prefix}space_invite_record`
+            ADD INDEX `idx_email`(`invite_email`) USING BTREE;
+        </sql>
+    </changeSet>
